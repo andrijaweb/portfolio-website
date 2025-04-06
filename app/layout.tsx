e73768e52451cde76ca29c87d5e579060c4a50ba
andrijaweb <andrijawebd@gmail.com>
2025-04-06 15:15:22 +0200

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/_styles/globals.css";
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import { ThemeProvider } from "./_components/ThemeProvider";
+import Script from "next/script";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -21,6 +22,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+      page_path: window.location.pathname,
+    });
+        `}
+        </Script>
+      </head>
       <body
         className={`${raleway.className} antialiased bg-white dark:bg-dark-100 text-dark-200 dark:text-stone-200`}
       >
